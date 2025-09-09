18bd52a6a81adc0c9f9662d52bc96703b786b1b0
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-09 20:19:23 +0200

    Updated code

--- pruebas.py
+++ pruebas.py
@@ -6,3 +6,13 @@ moneda = input("Introduce una moneda").upper()
 
 
 r = requests.get(f"https//api.exchangerate.host/live?acces_key={API_KEY}&source={moneda}&currencies=EUR,USD")
+
+
+answer = r.json()
+
+if r.status_code == 200:
+    print("rates: ",answer.get("quotes"))
+    value1 = moneda + "EUR"
+    value2 = moneda + "USD"
+else:
+    print("error: ",answer)    
